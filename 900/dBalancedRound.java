2b427df4359b89ad7c63e9270dcbe7226afecf62
Saksham Fulse <149682288+saksham6179@users.noreply.github.com>
2026-07-26 22:33:17 +0530

    Add dBalancedRound.java

---
+++ 900/dBalancedRound.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class dBalancedRound{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int[] problems = new int[n];
+            for(int i = 0;i<n;i++){
+                problems[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(problems);
+            int maxSeqCount = 0;
+            int currentSeqCount = 0;
+            for(int i = 1;i<n;i++){
+                if(problems[i]-problems[i-1]>k){
+                    maxSeqCount = Math.max(maxSeqCount,(currentSeqCount+1));
+                    currentSeqCount = 0;
+                }else{
+                    currentSeqCount++;
+                }
+            }
+            maxSeqCount = Math.max(maxSeqCount,(currentSeqCount+1));
+            System.out.println(n-maxSeqCount);
+        }
+    }
+}
